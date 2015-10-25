c4cc9a6ea4d6d2666efc9ded8c63d52729903023
nicofgomez <gomez.nicoo@gmail.com>
2015-10-25 13:30:55 -0300

    Subo el proyecto a GIT

---
+++ MVCPagina/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCPagina.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        } 
+
+        public ActionResult Mensaje()
+        {
+            ViewBag.Message = "El mail ha sido enviado correctamente.";
+            return View("Contact");
+        }
+        public  ActionResult Error()
+        {
+            ViewBag.Error = "Lo sentimos, en este momento el servidor no pudo enviar su consulta. Intente mas tarde por favor.";
+            return View("Contact");
+        }
+        public ActionResult EnviarMail(string name, string email, string phone, string message)
+        {
+            try
+            {
+                MailMessage mail = new MailMessage();
+                mail.From = new MailAddress(email);
+                mail.To.Add("andres.yk.ipod@gmail.com");
+                mail.Subject = "A&S Solutions. Consulta desde la página - " + phone;
+                string Body = message;
+                mail.Body = Body;
+                mail.IsBodyHtml = true;
+                SmtpClient smtp = new SmtpClient();
+                smtp.Send(mail);
+                return RedirectToAction("Mensaje");
+            }
+            catch (Exception e)
+            {
+                ViewBag.Message = "No se ha podido enviar el mensaje: " + e.Message;
+                return RedirectToAction("Error");
+            }
+        }
+
+    }
+}
